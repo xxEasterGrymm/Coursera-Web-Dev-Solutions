f3c5a94ae934bc093eb79877c014ba0807ed495f
unknown <you@example.com>
2021-09-11 00:00:32 +0530

    initial commit

---
+++ module4_solution/SpeakGoodBye.js
@@ -0,0 +1,12 @@
+// IIFE to create function that displays "Good Bye" + " " + "name". name will be passed as arguement.
+
+(function(window){
+	var byeSpeaker = {};
+	var speakWord = "Good Bye";
+	byeSpeaker.speak = function speak(name) {
+  		console.log(speakWord + " " + name);
+	}
+
+window.byeSpeaker = byeSpeaker;
+
+})(window);
